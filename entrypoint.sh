8ee650dcd6b563bcf002ce104c133f420ca824a6
peterdev22 <95014170+peterdev22@users.noreply.github.com>
2025-10-26 13:59:09 +1030

    Wait for unbound to start in entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,25 @@
-#!bin/bash
+#!/bin/bash
 
 # Exit on error
 set -e
 
 # Start unbound in the background
+echo "Starting unbound"
 /usr/sbin/unbound -c /etc/unbound/unbound.conf.d/pi-hole.conf &
+
+# Wait for unbound to start working
+wait_for=20
+timeout_at=$((SECONDS+wait_for))
+
+while ! dig @127.0.0.1 -p 5335 pi-hole.net >/dev/null 2>&1; do
+    sleep 0.1
+    if [ $SECONDS -ge $timeout_at ]; then
+        echo "Unbound did not start"
+        exit 1
+    fi
+done
+
+echo "Unbound has started"
 
 # Start Pi-hole using its start script
 exec /usr/bin/start.sh "$@"
